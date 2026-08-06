6ff569d8041acb7fa13ae96928443db03d744e3a
Vaibhava G <vaibhavg8121@gmail.com>
2026-08-06 18:50:23 +0530

    test firebase

--- src/firebase.js
+++ src/firebase.js
@@ -12,6 +12,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
+console.log(import.meta.env.VITE_FIREBASE_API_KEY);
+
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
